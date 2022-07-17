f3e53fc9000aa64d365723dd6fd585f53a117be9
nxoo <nxoo.dev@gmail.com>
2022-07-17 09:57:02 +0300

    daily milk shed production table

---
+++ Scripts/index.js
@@ -0,0 +1,24 @@
+const sheds = {
+    'one': {
+        'daily': [0],
+        'amount': 0,
+        'earned': 0
+    },
+    'two': {
+        'daily': [0],
+        'amount': 0,
+        'earned': 0
+    },
+    'three': {
+        'daily': [0],
+        'amount': 0,
+        'earned': 0
+    },
+    'four': {
+        'daily': [0],
+        'amount': 0,
+        'earned': 0
+    }
+}
+
+const buttonShed = document.getElementById("shedBbn")
