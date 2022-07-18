777ac81bc50f75dd0f65fc1ecbeda863769d2a06
nxoo <nxoo.dev@gmail.com>
2022-07-18 16:21:51 +0300

    initial data, and populating table columns

--- Scripts/index.js
+++ Scripts/index.js
@@ -6,9 +6,37 @@ const data = {
     ],
     // shed two
     'two': [
-        [12, 13, 10, 14, 9, 10, 11], // week 1
+        [12, 13, 10, 14, 9, 13, 9], // week 1
         [11, 16, 12, 12, 11, 10, 14] // week 2
     ]
 }
 
+console.log(data)
 
+const fillShedOne = () => {
+    const shedOne = document.getElementById("shedOne")
+    data.one[0].forEach(x => {
+        let td = document.createElement('td')
+        let tdValue = document.createTextNode(x)
+        td.appendChild(tdValue)
+        shedOne.appendChild(td)
+    })
+}
+
+const fillShedTwo = () => {
+    const shedTwo = document.getElementById("shedTwo")
+    data.two[0].forEach(x => {
+        let td = document.createElement('td')
+        let tdValue = document.createTextNode(x)
+        td.appendChild(tdValue)
+        shedTwo.appendChild(td)
+    })
+}
+
+
+const totalProduction  = () => {
+    fillShedOne()
+    fillShedTwo()
+}
+
+totalProduction()
