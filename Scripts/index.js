de96e9fdabd5320a52cf4b98c2dc8e5113d4a083
nxoo <nxoo.dev@gmail.com>
2022-07-18 14:54:45 +0300

    week navigator

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,24 +1,5 @@
-const sheds = {
-    'one': {
-        'daily': [0],
-        'amount': 0,
-        'earned': 0
-    },
-    'two': {
-        'daily': [0],
-        'amount': 0,
-        'earned': 0
-    },
-    'three': {
-        'daily': [0],
-        'amount': 0,
-        'earned': 0
-    },
-    'four': {
-        'daily': [0],
-        'amount': 0,
-        'earned': 0
-    }
-}
-
-const buttonShed = document.getElementById("shedBbn")
+const weeks = [
+    [
+        [10, 20, 30]
+    ]
+]
